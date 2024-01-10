071ce3bacdd7a76fddd3fa4a3ffcd4fc126b8bfa
Matthew Bomett <bomettmat2@gmail.com>
2024-01-10 18:24:22 +0300

    Completed Assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,19 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
+        # send a GET request to the URL
+        response = requests.get(self.url)
+
+        # return the response content
+        return response.content
         pass
 
     def load_json(self):
+        # get the response body
+        response_body = self.get_response_body()
+
+        # parse the JSON content
+        data = json.loads(response_body)
+
+        return data
         pass
